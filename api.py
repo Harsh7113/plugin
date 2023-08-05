2fa2548442570124251f6ccae26e2664256262ac
Harsh7113 <114785191+Harsh7113@users.noreply.github.com>
2023-08-05 12:20:03 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,103 @@
+import mysql.connector
+from fastapi import FastAPI, HTTPException, UploadFile
+from pydantic import BaseModel
+import uvicorn
+
+class Database:
+    def __init__(self, host, port, username, password, database):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database = database
+        self.connect()
+        
+
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            port=self.port,
+            user=self.username,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.connection.cursor()
+
+
+    def query_data(self, table_name: str):
+        query = f"SELECT * FROM {table_name};"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        return result
+
+    def upsert_data(self, data, table_name):
+        # will be implementing upsert logic here
+        pass
+
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query: str
+
+class UpsertRequest(BaseModel):
+    data: dict
+    table_name: str
+
+db = Database(
+    host="localhost",
+    port="3306",
+    username="root",
+    password="484661",
+    database="school"
+)
+
+@app.get("/query")
+def query_data(table_name: str):  
+    try:
+        result = db.query_data(table_name)  
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/upsert")
+def upsert_data(request: UpsertRequest):
+    try:
+        data = request.data
+        table_name = data.get("table_name")
+        
+        # extracts relevant fields from user request data and perform the update logic
+        if table_name in db.connection.table_names():
+            
+            # get the list of columns in the table
+            db.cursor.execute(f"DESCRIBE {table_name};")
+            columns = []
+            for column in db.cursor.fetchall():
+                columns.append(column[0])
+
+            # filter the input data to only include valid columns
+            filtered_data = {k: v for k, v in data.items() if k in columns}
+
+            # checks if the filtered data is not empty
+            if filtered_data:
+                
+                #constructs the SQL query dynamically based on the filtered data
+                placeholders = ', '.join(['%s'] * len(filtered_data))
+                columns_query = ', '.join(filtered_data.keys())
+                values = tuple(filtered_data.values())
+
+                #performs the INSERT or UPDATE query based on the table and existing records
+                db.cursor.execute(f"SELECT * FROM {table_name} LIMIT 1;")
+                existing_record = db.cursor.fetchone()
+
+                if existing_record:
+                    set_query = ', '.join([f"{column} = %s" for column in filtered_data.keys()])
+                    db.cursor.execute(f"UPDATE {table_name} SET {set_query};", values + values)
+                else:
+                    db.cursor.execute(f"INSERT INTO {table_name} ({columns_query}) VALUES ({placeholders});", values)
+
+                #makes the changes to the database
+                db.connection.commit()
+
+        return {"message": "Successfully updated data into the database"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ plugin.py
@@ -0,0 +1,55 @@
+import openai
+from pydantic import BaseModel
+from typing import Optional
+from fastapi import FastAPI, HTTPException
+import requests
+import uvicorn
+
+openai.api_key = "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"
+completion_model = "1.0.0"
+api_url = "http://localhost:8000"   
+
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query: str
+
+class UpsertRequest(BaseModel):
+    data: dict
+    table_name: str
+
+@app.post("/chat")
+async def chat(query: QueryRequest):
+    try:
+        response = requests.get(f"{api_url}/query?table_name=conversation_history")
+        result = response.json()
+
+        conversation_history = ""
+        for entry in result:
+            conversation_history += entry[0] + "\n" + entry[1] + "\n"
+
+        prompt = conversation_history + query.query
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=50,
+            model=completion_model
+        )
+        reply = response.choices[0].text.strip()
+
+        # this saves the conversation history
+        upsert_data = {
+            "data": {
+                "user_query": query.query,
+                "bot_reply": reply
+            },
+            "table_name": "conversation_history"
+        }
+
+        response = requests.post(f"{api_url}/upsert", json=upsert_data)
+        response.raise_for_status()
+        
+        return {"reply": reply}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
